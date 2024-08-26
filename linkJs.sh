2f1fefc163d28bc94d877a06cf66c03a5ecf0d1a
Kamil Enl <enlightenmentsoftware@gmail.com>
2024-08-26 11:23:41 +0200

    first commit

---
+++ linkJs.sh
@@ -0,0 +1,7 @@
+curl -O "https://cdn.jsdelivr.net/npm/node-forge@1.0.0/dist/forge.min.js"
+curl -O "https://cdn.jsdelivr.net/npm/node-forge@1.0.0/dist/prime.worker.min.js"
+curl -O "https://cdn.jsdelivr.net/npm/node-forge@1.0.0/dist/forge.all.min.js"
+curl -O "https://kjur.github.io/jsrsasign/jsrsasign-latest-all-min.js"
+
+curl -O "https://cdnjs.cloudflare.com/ajax/libs/forge/1.3.1/prime.worker.min.js"
+curl -O "https://cdnjs.cloudflare.com/ajax/libs/forge/1.3.1/forge.min.js"
